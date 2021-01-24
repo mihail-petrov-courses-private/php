e680510e43f05209bd436a96e0f7880d78aae58d
Mihail Petrov <mail@mihail-petrov.me>
2021-01-24 20:06:04 +0200

    Introduction to CMS concepts

---
+++ 2020-2021/iteration-1/weekdump/week-6-1/MyWebShopCMS/index.php
@@ -0,0 +1,3 @@
+<?php
+
+header('Location: http://localhost/MyWebShopCMS/view/front/');
---
+++ 2020-2021/iteration-1/weekdump/week-6-1/MyWebShopCMS/view/layout/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/vendor/grid.css"/>
+    <link rel="stylesheet" href="../../css/style.css"/>
+    <link rel="stylesheet" href="../../css/components/header.component.css"/>
+    <link rel="stylesheet" href="../../css/components/menu.component.css"/>
+</head>
+<body>
+    
+    <!-- BEM (BLOCK - ELEMENT - MODIFICATOR) -->
+    <div id="component--header">
+        
+        <div class="row">
+            <div class="col-xs-4">
+                <div id="component--header-logo">
+                    CMS WEB SHOP
+                </div>
+            </div>
+            
+            <div class="col-xs-8">
+                <div id="component--menu">
+                    <ul>
+                        <li>Начало</li>
+                        <li>Вход</li>
+                        <li>Регистрация</li>
+                    </ul>
+                </div>         
+            </div>
+        </div>
+    </div>
+    
+    
+    
