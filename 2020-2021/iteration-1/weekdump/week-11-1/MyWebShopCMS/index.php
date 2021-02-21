c2dd250407e5ea5118e735f070062ee66f6c05d9
Mihail Petrov <mail@mihail-petrov.me>
2021-02-21 21:46:47 +0200

    Use HTTP method as a way to redistibute controller method

---
+++ 2020-2021/iteration-1/weekdump/week-11-1/MyWebShopCMS/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include './src/vendor/util/utils.php';
+include basecontext('src/client/route/paths.php'          );
+include basecontext('src/client/components/dropdown.php'  );        
+include basecontext('src/client/route/guards.php'         ); 
+include basecontext('src/vendor/loader/Loader.php'        );        
+
+spl_autoload_register(function($class) {
+    include basecontext($class . '.php');
+});
+
+Loader::loadController();
